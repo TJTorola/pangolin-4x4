5f0ffc694d86b5128d98c540e8c6efb8bc75038f
Tyler Torola <tyler@tjt.codes>
2017-05-18 18:51:28 -0700

    Add css build framework

---
+++ css/build.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const postcss = require('postcss');
+
+const atImport = require('postcss-import');
+const autoPrefix = require('autoprefixer');
+const customProperties = require('postcss-custom-properties');
+const customMedia = require('postcss-custom-media');
+const calc = require('postcss-calc');
+const cssnano = require('cssnano');
+
+const indexCss = fs.readFileSync('index.css', 'utf8');
+
+postcss([
+  atImport(),
+  autoPrefix(),
+  customProperties(),
+  customMedia(),
+  calc(),
+  cssnano({
+    discardComments: {
+      removeAll: true,
+    },
+  }),
+]).process(indexCss, {
+  from: 'index.css',
+  to: '../dist/style.css',
+}).then(result => fs.writeFile('../dist/style.css', result.css));
