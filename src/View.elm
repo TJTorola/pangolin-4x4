0efa8e03e00638479b67968f348e58f8881bebbd
Tyler Torola <tyler@tjt.codes>
2017-05-20 23:08:56 -0700

    Impliment basic layout

--- src/View.elm
+++ src/View.elm
@@ -4,9 +4,42 @@ import Model exposing (Model)
 import Actions exposing (Action)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+
+
+-- import Html.Events exposing (..)
 
 
 view : Model -> Html Action
 view model =
-    div [] [ text "Penguin 2xxx" ]
+    div [ id "App" ]
+        [ viewHeader model
+        , div [ id "Page" ] []
+        , viewFooter model
+        ]
+
+
+viewHeader : Model -> Html Action
+viewHeader model =
+    div [ id "Header" ]
+        [ div [ id "Header-highlightBar" ] []
+        , div [ id "Header-bodyWrapper" ]
+            [ div [ id "Header-body" ]
+                [ div [ id "Header-logo" ] []
+                , div [ id "Header-copy" ] []
+                ]
+            ]
+        , div [ id "Header-navWrapper" ]
+            [ div [ id "Header-nav" ]
+                [ div [ id "Header-links" ] []
+                , div [ id "Header-search" ] []
+                ]
+            ]
+        ]
+
+
+viewFooter : Model -> Html Action
+viewFooter model =
+    div [ id "Footer" ]
+        [ div [ id "Footer-blackBar" ] []
+        , div [ id "Footer-body" ] []
+        ]
