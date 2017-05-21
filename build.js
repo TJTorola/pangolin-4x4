35da502a5341f1d0e5c6bcda231709fefe64aeb6
Tyler Torola <tyler@tjt.codes>
2017-05-20 23:25:35 -0700

    Clean up build scripts

--- build.js
+++ build.js
@@ -1,5 +1,35 @@
 const spawn = require('child_process').spawn;
-const buildCss = require('./css/build.js');
+const postcssWatch = require('postcss-watch');
+
+// Build CSS
+
+const atImport = require('postcss-import');
+const autoPrefix = require('autoprefixer');
+const customProperties = require('postcss-custom-properties');
+const customMedia = require('postcss-custom-media');
+const calc = require('postcss-calc');
+const cssnano = require('cssnano');
+
+const plugins = [
+  atImport(),
+  autoPrefix(),
+  customProperties(),
+  customMedia(),
+  calc(),
+  cssnano({
+    discardComments: {
+      removeAll: true,
+    },
+  }),
+];
+
+postcssWatch({
+  input: "css",
+  output: "dist",
+  plugins,
+});
+
+// Build Elm
 
 const BUILD_ELM_EXEC = './node_modules/.bin/elm-live';
 const BUILD_ELM_ARGS = [
@@ -11,5 +41,3 @@ const BUILD_ELM_ARGS = [
 
 const elmBuilder = spawn(BUILD_ELM_EXEC, BUILD_ELM_ARGS);
 elmBuilder.stdout.pipe(process.stdout);
-
-buildCss();
